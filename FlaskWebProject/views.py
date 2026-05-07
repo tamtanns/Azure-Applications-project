38f668289d25b006b99f13244c9f1276c9010802
fixc0mm <ammar.shikh.khalil@cariad.technology>
2026-05-07 12:04:57 +0200

    correct the content of views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -13,7 +13,7 @@ from FlaskWebProject.models import User, Post
 import msal
 import uuid
 
-imageSourceUrl = 'https://'+ current_app.config['BLOB_ACCOUNT']  + '.blob.core.windows.net/' + current_app.config['BLOB_CONTAINER']  + '/'
+imageSourceUrl = 'https://' + app.config['BLOB_ACCOUNT'] + '.blob.core.windows.net/' + app.config['BLOB_CONTAINER'] + '/'
 
 @app.route('/')
 @app.route('/home')
