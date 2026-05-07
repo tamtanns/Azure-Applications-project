9149bcd57ae2f4b91b1c55263c5acfc40d044e82
fixc0mm <ammar.shikh.khalil@cariad.technology>
2026-05-07 12:12:23 +0200

    Fix HTTPS redirect URI in views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -96,7 +96,7 @@ def authorized():
         result = msal_app.acquire_token_by_authorization_code(
             request.args["code"],
             scopes=Config.SCOPE,
-            redirect_uri=url_for("authorized", _external=True),
+            redirect_uri=url_for("authorized", _external=True, _scheme="https"),
         )
         if "error" in result:
             app.logger.warning(
