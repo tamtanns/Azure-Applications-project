646d77abf8771aa1d3e0ad9ec01a1fd477279937
fixc0mm <ammar.shikh.khalil@cariad.technology>
2026-05-07 13:05:17 +0200

    add streamhandler with app.logger in init.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,10 +10,14 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 # TODO: Add any logging levels and handlers with app.logger
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+app.logger.addHandler(stream_handler)
+app.logger.setLevel(logging.INFO)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
-
-import FlaskWebProject.views
